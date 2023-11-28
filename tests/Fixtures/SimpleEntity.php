490700260eb7f0c8fd52385f57fc27dd698481bd
Petr Chromec <mortal_flesh@seznam.cz>
2023-11-28 12:20:14 +0100

    Add IList::partition method

--- tests/Fixtures/SimpleEntity.php
+++ tests/Fixtures/SimpleEntity.php
@@ -4,6 +4,11 @@ namespace MF\Collection\Fixtures;
 
 class SimpleEntity implements EntityInterface
 {
+    public static function create(int $id): self
+    {
+        return new self($id);
+    }
+
     public function __construct(private int $id)
     {
     }
